5a7bad40db37ca2650bc6ebf5c56311ddbf86869
CyberTomB <tomcbowers@gmail.com>
2025-01-10 11:25:26 -0700

    add tests to assert value error when no tag or no children for parentnode

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -12,15 +12,21 @@ class TestParentNode(unittest.TestCase):
         node = ParentNode(tag="p", children=[LeafNode("b", "BOLD TEXT"), LeafNode("i", "ITALIC TEXT")])
         self.assertEqual(node.to_html(), '<p><b>BOLD TEXT</b><i>ITALIC TEXT</i></p>')
 
-
-
-    def test_to_html_no_children(self):
+    def test_to_html_with_empty_children_list(self):
         node = ParentNode(tag="p", children=[])
         self.assertEqual(node.to_html(), '<p></p>')
 
     def test_to_html_with_children_of_children(self):
         node = ParentNode(tag="body", children=[ParentNode(tag="p", children=[LeafNode("b", "BOLD TEXT")])])
         self.assertEqual(node.to_html(), '<body><p><b>BOLD TEXT</b></p></body>')
+
+    def test_raise_value_error_no_tag(self):
+        node = ParentNode(tag=None, children=[])
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_raise_value_error_none_children(self):
+        node = ParentNode(tag="p", children=None)
+        self.assertRaises(ValueError, node.to_html)
 
 
 
