132fc8d8373544f44332839900b74b3b07d54737
CyberTomB <tomcbowers@gmail.com>
2025-01-11 23:08:39 -0700

    WIP - create textnode delimiter method

--- src/conversions.py
+++ src/conversions.py
@@ -2,6 +2,7 @@ from textnode import TextNode, TextType
 from leafnode import LeafNode
 
 def text_node_to_html_node(text_node: TextNode):
+
     value, type = text_node.text, text_node.text_type
 
     match type:
@@ -24,4 +25,18 @@ def text_node_to_html_node(text_node: TextNode):
             })
         case _:
             return value
-    
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter, text_type):
+    final_nodes = []
+    for node in old_nodes:
+        new_nodes = []
+        split_text = node.text.split(delimiter)
+        for i in range(len(split_text)):
+            if not i % 2 == 0:
+                print(">>Non-even iteration")
+                new_nodes.append(TextNode(split_text[i], text_type=text_type))
+            else:
+                new_nodes.append(TextNode(split_text[i], text_type="text"))
+        final_nodes.extend(new_nodes)
+    return final_nodes
+        
--- src/test_conversions.py
+++ src/test_conversions.py
@@ -46,6 +46,16 @@ class TestConversions(unittest.TestCase):
         self.assertEqual(f"<img src=\"www.url.com\" alt=\"{text}\"></img>", node.to_html())
         self.assertIsInstance(node, LeafNode)
 
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("This will have a **bold** markdown.", "text")
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(result), 3)
+        self.assertIsInstance(result[0], TextNode)
+        self.assertEqual(result[0].text, "This will have a ")
+        self.assertEqual(result[1].text, "bold")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, " markdown.")
+
 
 if __name__ == "__main__":
     unittest.main()
